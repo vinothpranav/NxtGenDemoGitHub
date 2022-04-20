2e07a34709c53b704bdd7b39db33df8bec5c6101
Vinoth Rathinam <vinothrwins@gmail.com>
2022-04-20 21:02:38 +0530

    Code Added by Automation team

--- NxtGenDemoGitHub/src/com/nxtgenai/github/WelcomeUser.java
+++ NxtGenDemoGitHub/src/com/nxtgenai/github/WelcomeUser.java
@@ -5,6 +5,9 @@ public class WelcomeUser {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Github Class");
+		
+		// Code added by automation team
+		System.out.println("Thanks you");
 	}
 
 }
